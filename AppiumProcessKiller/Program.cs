c90e33f89bf9cfc6362eabe8de85293fe8e717c0
Rajeev Barde <rajeevv67@yahoo.com>
2020-09-13 23:26:15 +0530

    first commit

---
+++ AppiumProcessKiller/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+
+namespace AppiumProcessKiller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isFound = false;
+
+            foreach (Process PPath in Process.GetProcessesByName("node"))
+            {                
+                int processId = PPath.Id;
+                string processArgs = "";
+
+                var q = string.Format("select CommandLine from Win32_Process where ProcessId='{0}'", processId);
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(q);
+                ManagementObjectCollection result = searcher.Get();
+
+                foreach (ManagementObject obj in result)
+                {                    
+                    processArgs = obj["CommandLine"].ToString();
+                    if (processArgs.ToLower().Contains("appium"))
+                    {
+                        isFound = true;
+                        PPath.Kill();
+                        Console.WriteLine("Killed process id {0} : {1}", processId, processArgs);
+                    }     
+                }                
+            }
+
+            if (!isFound)
+                Console.WriteLine("No process found for appium");            
+        }
+    }
+}
